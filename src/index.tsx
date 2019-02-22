f2e267f4f2fe4a7866dd984be2551ca018d72f2c
Subramanian <subbu@furhatrobotics.com>
2019-02-22 18:25:16 +0100

    First Commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <div>Hello World!!!</div>,
+    document.getElementById("root")
+);
+
+// https://hackernoon.com/react-with-typescript-and-webpack-654f93f34db6
